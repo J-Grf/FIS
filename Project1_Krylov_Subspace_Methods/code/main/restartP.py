ffd8bc974ba4182d302107feeafb2a48a3e4b7d7
Johannes Grafen <johannes.grafen@rwth-aachen.de>
2023-06-04 15:33:51 +0200

    fixes

--- Project1_Krylov_Subspace_Methods/code/main/restartP.py
+++ Project1_Krylov_Subspace_Methods/code/main/restartP.py
@@ -3,24 +3,26 @@
 import subprocess as sp
 import re
 
-m_min = 232
-m_max = 233
+m_min = 20
+m_max = 501
 time = []
 iterations = []
-count = 3
+count = 0
+timingsDir = "./timings/"
 
 for m in range(m_min, m_max):
     print("Running m = " + str(m))
-    with open(str(m) + ".txt", "w") as f:
+    with open(timingsDir + str(m) + ".txt", "w") as f:
         sp.run(["./main.exe", "GMRES", "../../material/gmres_test_msr.txt", str(m), "NONE"], stdout=f)
     
-    with open(str(m) + ".txt", "r") as f:
+    with open(timingsDir + str(m) + ".txt", "r") as f:
         data = f.readlines()
         for line in data:
             
             # check if we enter restarted GMRES
             count += 1
             if count == 3 and not re.match(r"restart", line):
+                print("for m = " + str(m) + " no  restart necessary, collected all timings")
                 break
 
             match = re.match(r".* = ([+-]?([0-9]*[.])?[0-9]+)s CPU .*", line)
@@ -31,6 +33,7 @@ for m in range(m_min, m_max):
             match2 = re.match(r"m = (\d+) iterations = (\d+).*", line)
             if match2:
                 iterations.append(int(match2.group(2)))
+    count = 0
 
 MIN = min(time)
 IDX = time.index(MIN) + m_min
