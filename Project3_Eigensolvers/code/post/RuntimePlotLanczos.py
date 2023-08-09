4892a656c6cbc290adb1dd88101faa2ad5e7f53d
Johannes Grafen <johannes.grafen@rwth-aachen.de>
2023-08-09 23:30:00 +0200

    postprocessing scripts improved and finalized

--- Project3_Eigensolvers/code/post/RuntimePlotLanczos.py
+++ Project3_Eigensolvers/code/post/RuntimePlotLanczos.py
@@ -28,6 +28,7 @@ for i in m:
 #read timings-file
 times = []
 lambdas = []
+errors = []
 with open(path, 'r') as f:
     data = f.readlines()
     for line in data:
@@ -36,8 +37,9 @@ with open(path, 'r') as f:
             print(times)
             times.append(float(match[0]))
             lambdas.append(float(match[1]))
+            errors.append(float(match[2]))
 
-print(lambdas)
+#print(lambdas)
 
 def PlotSemiLog(name, x, y, xlab, ylab, x_min = None):
     fig = plt.figure(name)
@@ -50,12 +52,12 @@ def PlotSemiLog(name, x, y, xlab, ylab, x_min = None):
         ax.set_xlim(xmin = x_min)
     
     ax.set_xlabel(xlab, fontsize=fs)
-    ax.set_ylabel(ylab, fontsize=fs, labelpad=35, rotation = 0)
+    ax.set_ylabel(ylab, fontsize=fs, labelpad=40, rotation = 0)
     
-    if "Errors" in name:
+    if "Lambdas" in name:
         plt.axhline(y = 9.5986080894852857E3, color = 'r', linestyle='-')
         ax.set_yticks([9.584E3, 9.586E3, 9.588E3, 9.59E3, 9.592E3, 9.594E3, 9.596E3, 9.598E3])
-    else:
+    elif "Timings" in name:
         ax.set_yticks([4E-2, 6E-2, 8E-2, 1E-1, 2E-1])
         ax.set_yticklabels([4E-2, 6E-2, 8E-2, 1E-1, 2E-1])
     ax.grid()
@@ -64,6 +66,7 @@ def PlotSemiLog(name, x, y, xlab, ylab, x_min = None):
     pgf.savePgf(pgfDir + name + ".pgf", factor = 0.8)
 
 PlotSemiLog("Lanczos_Timings", m, times, r"$m$", r"$t \ [s]$")
-PlotSemiLog("Lanczos_Errors", m, lambdas, r"$m$", r"$\lambda_{max}$")
+PlotSemiLog("Lanczos_Lambdas", m, lambdas, r"$m$", r"$\lambda_{max}$")
+PlotSemiLog("Lanczos_Errors", m, errors, r"$m$", r"$|\lambda_{cg}^{ex} - \lambda^{(k)}|$")
 
     
--- Project3_Eigensolvers/code/post/convergencePlotsPowerIt.py
+++ Project3_Eigensolvers/code/post/convergencePlotsPowerIt.py
@@ -3,23 +3,25 @@
 import matplotlib.pyplot as plt
 import re
 import pgf
+import sys
 
 plotDir = "/Users/johannes/Desktop/FIS/projects/Project3_Eigensolvers/plots/"
 pgfDir = "/Users/johannes/Desktop/FIS/projects/Project3_Eigensolvers/report/plots/"
 
-regexFloat = re.compile(r'([-+]?[0-9]*[.][0-9]+(?:[eE][-+]?\d+)?)')
+regexFloat = re.compile(r'([-+]?[0-9]*[.]?[0-9]+(?:[eE][-+]?\d+)?)')
 fs = 12
 
-def PlotSemiLog(name, x, y, xlab, x_min, ylab):
+def PlotSemiLog(name, x, y, xlab, x_min, ylab, x_max):
     fig = plt.figure(name)
     fig.clear()
     ax = fig.gca()
 
     ax.semilogy(x, y)
-    ax.set_ylim(ymax = 100, ymin=1E-12)
-    ax.set_xlim(xmin = x_min)
+    #ax.set_ylim(ymax = 1E7, ymin=1E-10)
+    ax.set_ylim(ymax = 1E4, ymin = 1E-8)
+    ax.set_xlim(xmin = x_min, xmax = x_max)
     ax.set_xlabel(xlab, fontsize=fs)
-    ax.set_ylabel(ylab, fontsize=fs, labelpad=35, rotation = 0)
+    ax.set_ylabel(ylab, fontsize=fs, labelpad=40, rotation = 0)
     ax.grid()
     plt.tight_layout()
     plt.savefig(plotDir + name + ".pdf", dpi = 100)
@@ -31,7 +33,7 @@ time = []
 diff = []
 error = []
 i = 0
-with open("../../rawData/powerIterationConvergence.txt") as f:
+with open(sys.argv[1]) as f:
     content = f.readlines()
     for line in content:
         print(line)
@@ -39,16 +41,17 @@ with open("../../rawData/powerIterationConvergence.txt") as f:
         if(len(match) > 0):
             i+=1
             it.append(i)
-            time.append(float(match[0]))
-            diff.append(float(match[1]))
-            error.append(float(match[2]))
+            time.append(float(match[1]))
+            diff.append(float(match[2]))
+            error.append(float(match[3]))
 
 ylab1 = r"$|\lambda^{(k)} - \lambda^{(k-1)}|$"
-PlotSemiLog("PowerIt_1_diff", it, diff, r"$k$", 1, ylab1)
-PlotSemiLog("PowerIt_2_diff", time, diff, r"$t \ [s]$", 0, ylab1)
+print(diff)
+PlotSemiLog("PowerIt_test_convergence", it, diff, r"$k$", 1, ylab1, 2200)
+PlotSemiLog("PowerIt_2_diff", time, diff, r"$t \ [s]$", 0, ylab1, 3.5)
 
 ylab2 = r"$|\lambda_{cg}^{ex} - \lambda^{(k)}|$"
-PlotSemiLog("PowerIt_1_error", it, error, r"$k$", 1, ylab2)
-PlotSemiLog("PowerIt_2_error", time, error, r"$t \ [s]$", 0, ylab2)
+PlotSemiLog("PowerIt_1_error", it, error, r"$k$", 1, ylab2, 2200)
+PlotSemiLog("PowerIt_2_error", time, error, r"$t \ [s]$", 0, ylab2, 3.5)
 
 
